1a2e344eb1b01590e967004e53e378d81d2f4c93
ryan luca <58185393+ryan1235@users.noreply.github.com>
2022-05-01 17:42:15 -0300

    resolvendo bugs

--- src/components/paramount/newheade/NewHeade.js
+++ src/components/paramount/newheade/NewHeade.js
@@ -9,6 +9,7 @@ export default function NewHeader() {
           <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
           <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
           <meta name="description" content="Um site sobre meu trabalho em front-end" />
+          <meta name="description:Image" content="https://cdn.iconscout.com/icon/premium/png-256-thumb/innovative-technology-738852.png" />
           <link rel="icon" href="/favicon.ico" />
       </Head>
     )
